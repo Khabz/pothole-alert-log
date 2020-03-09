00bcac2bd3535e04f26a2a3f86af85ee32e1d71d
Khabz <khabubundivhu@gmail.com>
2020-03-09 20:58:30 +0200

    Dependecies installed. Linter setup

---
+++ server/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3301;
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+})

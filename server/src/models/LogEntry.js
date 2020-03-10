8bedb3906318e9e29855db0520216ec031c9c375
Khabz <khabubundivhu@gmail.com>
2020-03-10 12:05:35 +0200

    Server side completed :boom:

--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -1,47 +1,42 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
 
-const requiredString = {
-    type: String,
-    required: true
-};
+const { Schema } = mongoose;
 
 const requiredNumber = {
-    type: Number,
-    required: true
+  type: Number,
+  required: true,
 };
 
-var logEntrySchema = new Schema({
-    title: requiredString,
-    decription: String,
-    image: requiredString,
-    comments: {
-        type: String
-    },
-    rating: {
-        type: Number,
-        min: 0,
-        max: 10,
-        default: 0
-    },
-    latitude: {
-        ...requiredNumber,
-        min: -90,
-        max: 90
-    },
-    longitude: {
-        ...requiredNumber,
-        min: -180,
-        max: 180
-    },
-    created_by: requiredString,
-    fixed: {
-        type: Boolean,
-        default: false,
-        required: true
-    }
+const logEntrySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  comments: String,
+  image: String,
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+    default: 0,
+  },
+  latitude: {
+    ...requiredNumber,
+    min: -90,
+    max: 90,
+  },
+  longitude: {
+    ...requiredNumber,
+    min: -180,
+    max: 180,
+  },
+  visitDate: {
+    required: true,
+    type: Date,
+  },
 }, {
-    timestamps: true,
+  timestamps: true,
 });
 
 const LogEntry = mongoose.model('LogEntry', logEntrySchema);

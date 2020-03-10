dc69e1fb3c7bbf200429bbc3b9320b021cf38122
Khabz <khabubundivhu@gmail.com>
2020-03-10 17:33:44 +0200

    :rocket: client side created

--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -3,40 +3,38 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const requiredNumber = {
-  type: Number,
-  required: true,
+    type: Number,
+    required: true,
 };
 
 const logEntrySchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: String,
-  comments: String,
-  image: String,
-  rating: {
-    type: Number,
-    min: 0,
-    max: 10,
-    default: 0,
-  },
-  latitude: {
-    ...requiredNumber,
-    min: -90,
-    max: 90,
-  },
-  longitude: {
-    ...requiredNumber,
-    min: -180,
-    max: 180,
-  },
-  visitDate: {
-    required: true,
-    type: Date,
-  },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: String,
+    latitude: {
+        ...requiredNumber,
+        min: -90,
+        max: 90,
+    },
+    longitude: {
+        ...requiredNumber,
+        min: -180,
+        max: 180,
+    },
+    fixed: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    created_by: {
+        type: String,
+        required: true
+    }
 }, {
-  timestamps: true,
+    timestamps: true,
 });
 
 const LogEntry = mongoose.model('LogEntry', logEntrySchema);
